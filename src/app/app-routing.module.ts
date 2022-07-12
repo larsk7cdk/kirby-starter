b10de47093aaaebdce829ee747d3d9704a36bfe7
lal-jyskebank-dk <lal@jyskebank.dk>
2022-07-12 13:28:14 +0200

    initial commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./features/xxx/xxx.module').then((m) => m.xxxModule),
+  // },
+  { path: '**', redirectTo: 'home', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { KirbyModule } from '@kirbydesign/designsystem';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import localeDa from '@angular/common/locales/da';
+import { HomeModule } from './features/home/home.module';
+
+registerLocaleData(localeDa);
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, AppRoutingModule, KirbyModule, HomeModule],
+  providers: [{ provide: LOCALE_ID, useValue: 'da-DK' }],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
